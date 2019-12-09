1d08ca5e9e745da1893ed66e58467a048ed9f0d3
arahosu <arahosu@gmail.com>
2019-12-09 15:47:16 +0000

    adding merge files

---
+++ src/main/java/GUI/Overall_Graph.java
@@ -0,0 +1,66 @@
+package GUI;
+
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Overall_Graph {
+    private JFXPanel graph_panel;  //Overall panel with the four graphs
+    private Graph graphECG, graphBPress, graphResp, graphTemp;  //These are the four different graphs
+    private Thread refreshing1,refreshing2, refreshing3, refreshing4;  //These creates four different threads
+
+    public Overall_Graph() {
+
+        double[] sin_array = new double[13000];
+        int index_counter = 0;
+        for (double i = 0; i < 1299.9; i += 0.1) {
+            sin_array[index_counter] = Math.sin(i);
+            index_counter += 1;
+        }
+
+        graph_panel = new JFXPanel();
+        graph_panel.setLayout(new GridLayout(4, 1));
+
+        graphECG = new Graph("chart-ECG");
+        graphBPress = new Graph("chart-Pressure");
+        graphResp = new Graph("chart-Respiratory");
+        graphTemp = new Graph("chart-Temperature");
+
+        graph_panel.add(graphECG.getGraph());
+        graph_panel.add(graphBPress.getGraph());
+        graph_panel.add(graphResp.getGraph());
+        graph_panel.add(graphTemp.getGraph());
+
+        graphECG.setGraph(sin_array);
+        graphBPress.setGraph(sin_array);
+        graphResp.setGraph(sin_array);
+        graphTemp.setGraph(sin_array);
+
+    }
+
+    public void updatePanel() {
+        //Running four separate threads, one for each graph
+        refreshing1 = new Thread (new Refresh (graphECG));
+        refreshing2 = new Thread (new Refresh (graphBPress));
+        refreshing3 = new Thread (new Refresh (graphResp));
+        refreshing4 = new Thread (new Refresh (graphTemp));
+
+        //This makes sure that the program doesn't have to wait for each thread to be run
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run () {
+                refreshing1.start();
+                refreshing2.start();
+                refreshing3.start();
+                refreshing4.start();
+            }
+        });
+    }
+
+    public JFXPanel getGraphPanel() {
+        return graph_panel;
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import simulation.ECG;
+import java.util.Arrays;
+
+import GUI.Main_Frame;
+import GUI.UserLogIn;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+
+        //UserLogIn welcome = new UserLogIn();
+        Main_Frame summary = new Main_Frame();
     }
 }
