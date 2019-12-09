89c698aa2c7ba7899caede7ce0ed8b5be9a7fb19
arahosu <arahosu@gmail.com>
2019-12-09 15:10:15 +0000

    updated the repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,23 @@
+import org.apache.commons.lang3.ArrayUtils;
 import simulation.ECG;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        double variance = 0.0001;
+        double mean = 0;
+
+        ECG simulation = new ECG();
+        double[] array = simulation.Simulate();
+        System.out.println(Arrays.toString(array));
+        simulation.addNoise(mean, variance);
+
+        double[] zero_padding = new double[10];
+        double[] combined_array = ArrayUtils.addAll(zero_padding,array);
+
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(combined_array));
     }
-
-    ECG simulation = new ECG();
-
 }
+
 
--- src/main/java/simulation/ECG.java
+++ src/main/java/simulation/ECG.java
@@ -1,16 +1,30 @@
 package simulation;
 
+import java.util.Random;
+
 public class ECG {
 
-    private double[] array;
+    private double[] array; //array of one heartbeat
     /**
      * Constructor Class. Initialise ECG simulation parameters to default values
      */
-
     public ECG() {
         int order = 10;
         DaubechiesWavelet.SetOrder(order);
         array = DaubechiesWavelet.ReturnDaub();
+    }
+
+    public void addNoise(double mean, double variance)
+    {
+        int len = array.length;
+
+        for (int i = 0; i < len; i++)
+        {
+            Random r = new Random();
+            double noise = r.nextGaussian() * Math.sqrt(variance) + mean;
+
+            array[i] += noise;
+        }
     }
 
     public void SetDaubCoefficient(int n)
