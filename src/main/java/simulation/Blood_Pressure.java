2231b34d5f2e286b2f6ae46c6baa6aa575d851c7
Joearrowsmith <joearrowsmith98@gmail.com>
2019-12-11 18:26:56 +0000

    adding fixed arrays

--- src/main/java/simulation/Blood_Pressure.java
+++ src/main/java/simulation/Blood_Pressure.java
@@ -2,11 +2,22 @@ package simulation;
 
 public class Blood_Pressure {
 
+    double []array;
     int period;
     int counter;
     double t1, t2, t3;
 
     public Blood_Pressure(){
+        t1 = 0.60;
+        t2 = 0.15;
+        t3 = 0.25;
+        counter = 0;
+        period = 100;
+    }
+
+
+    public Blood_Pressure(int array_size){
+        array = new double[array_size];
         t1 = 0.60;
         t2 = 0.15;
         t3 = 0.25;
@@ -27,5 +38,15 @@ public class Blood_Pressure {
             double start = counter_period - period * (t1+t2);
             return -Math.sin(start/15)*27+107;
         }
+    }
+
+    private void fill_array(){
+        for (int i = 0; i < array.length - 1; i++){
+            array[i] = get_next_value();
+        }
+    }
+
+    public double[] get_array(){
+        return array;
     }
 }
