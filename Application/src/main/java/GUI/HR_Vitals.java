7ec45cc8df15fab72f4c716782fed61a19f29920
laporastrelli <lr4617@ic.ac.uk>
2019-12-12 10:17:06 +0000

    Updating GUI

--- Application/src/main/java/GUI/HR_Vitals.java
+++ Application/src/main/java/GUI/HR_Vitals.java
@@ -7,7 +7,7 @@ public class HR_Vitals extends Vital_Values_Display {
     public HR_Vitals(Dimension vitals_panel_dim){
         super(vitals_panel_dim, "label-Temperature");
         super.status_msg.setText("STABLE");
-        super.vital_type.setText("H\nR");
+        super.vital_type.setText("HR");
         super.vital_value.setText("VALUE");
     }
 
