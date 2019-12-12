9c6ed51999b58faa54f2e45789272551b5fe907a
pv09 <44298046+pv09@users.noreply.github.com>
2019-12-12 11:03:07 +0000

    changed axis and temp display

--- Application/src/main/java/GUI/HR_Vitals.java
+++ Application/src/main/java/GUI/HR_Vitals.java
@@ -19,7 +19,7 @@ public class HR_Vitals extends Vital_Values_Display {
 
     protected void Set_Displayed_Value(){
         Platform.runLater(() ->{
-            super.vital_value.setText(String.valueOf((int)(temp_val_counter.Double_Value())));
+            super.vital_value.setText(String.valueOf(Math.round(temp_val_counter.Double_Value() * 1000.0)/1000.0));
         });
     }
 }
