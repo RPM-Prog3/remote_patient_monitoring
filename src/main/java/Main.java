51d0b1c2b35c64e38522eae51d700d841393b3fb
arahosu <arahosu@gmail.com>
2020-01-10 14:15:42 +0000

    shit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@ import GUI.Main_Frame;
 import GUI.UserLogIn;
 
 public class Main {
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
         //UserLogIn welcome = new UserLogIn();
         Main_Frame summary = new Main_Frame();
-    }
-}
+        }
+        }
