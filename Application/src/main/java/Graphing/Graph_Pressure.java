ac9b9847796bb233f12ac60a5b913a865ad969a9
arahosu <arahosu@gmail.com>
2020-01-11 17:58:20 +0000

    added the option to vary blood pressure

--- Application/src/main/java/Graphing/Graph_Pressure.java
+++ Application/src/main/java/Graphing/Graph_Pressure.java
@@ -5,18 +5,20 @@ import simulation.Value_Counter;
 
 public class Graph_Pressure extends Graph {
     private Blood_Pressure pressuredata;
+    private int bp_type;
 
     public Graph_Pressure(String colorGraph, Value_Counter obj, double sample_period, int time_shown, Blood_Pressure pressuredata_input){
         super(colorGraph, obj, sample_period, time_shown);
         pressuredata = pressuredata_input;
+        bp_type = 0;
     }
 
     public void changeAbnormality(int newType) {
-
+        bp_type = newType;
     }
 
     protected void Get_Next_Value(){
-        super.data_point = pressuredata.get_next_value();
+        super.data_point = pressuredata.get_next_value(bp_type);
     }
 
     protected void Monitoring_Value(){
