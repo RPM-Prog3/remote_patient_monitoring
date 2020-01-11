12be9e166a504a9968c7403888a557266e662344
pv09 <44298046+pv09@users.noreply.github.com>
2020-01-11 23:27:54 +0000

    added conditions of urgent and stable to the blinking thread

--- Application/src/main/java/GUI/Blinking.java
+++ Application/src/main/java/GUI/Blinking.java
@@ -5,26 +5,61 @@ import javafx.scene.control.Label;
 
 public class Blinking implements Runnable {
     Label status;
+    private int which_status;
 
     public Blinking(Label status){
         this.status = status;
+        which_status = 0;
     }
 
     public void run(){
         try{
             int i = 0;
-            boolean blinking_boolean = false;
+            boolean remove_class = false;
+
             while(1<2){
-                if(blinking_boolean)
+                if (which_status == 0){
+                    if (remove_class)
+                        status.getStyleClass().remove(4);
+                    remove_class = false;
+                }
+
+                if (which_status == 1) {
+                    if (remove_class)
+                        status.getStyleClass().remove(4);
+                    status.getStyleClass().add("label-status-warning");
+                    Thread.sleep(1000);
                     status.getStyleClass().remove(4);
-                status.getStyleClass().add("label-status-warning");
-                Thread.sleep(1000);
-                status.getStyleClass().remove(4);
-                status.getStyleClass().add("label-status-stable");
-                Thread.sleep(1000);
-                blinking_boolean = true;
+                    status.getStyleClass().add("label-status-stable");
+                    Thread.sleep(1000);
+                    remove_class = true;
+                }
+
+                if (which_status == 2){
+                    if (remove_class)
+                        status.getStyleClass().remove(4);
+                    status.getStyleClass().add("label-status-warning");
+                    Thread.sleep(1000);
+                    status.getStyleClass().remove(4);
+                    status.getStyleClass().add("label-status-stable");
+                    Thread.sleep(1000);
+                    remove_class = true;
+                }
             }
 
         }catch (Exception e) {};
     }
+
+    public void stable_status(){
+        which_status = 0;
+    }
+
+    public void warning_status(){
+        which_status = 1;
+    }
+
+    public void urgent_status(){
+        which_status = 2;
+    }
+
 }
