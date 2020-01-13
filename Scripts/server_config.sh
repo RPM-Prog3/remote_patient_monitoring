96093dc52d9552e3b142317c696ccbdb74ebb78f
Joearrowsmith <joearrowsmith98@gmail.com>
2020-01-13 08:53:29 +0000

    testing test

--- Scripts/server_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ..
->db_config.properties
-echo "db_name=travis_test_db" >> db_config.properties
-echo "db_user_name=postgres" >> db_config.properties
-echo "db_password=admin" >> db_config.properties
->server_config.properties
